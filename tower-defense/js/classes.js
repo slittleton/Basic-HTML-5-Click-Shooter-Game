1f43d3440589f01b770be360a9bc5e9a2e605665
slittleton <littletonltp@gmail.com>
2023-05-04 16:33:31 -0700

    enemies and projectiles

--- tower-defense/js/classes.js
+++ tower-defense/js/classes.js
@@ -38,12 +38,30 @@ class Enemy {
             x: this.position.x + this.width / 2,
             y: this.position.y + this.height / 2
         };
+        this.radius = 50;
+        this.health = 100
     }
 
     draw() {
         c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+        // c.fillRect(this.position.x, this.position.y, this.width, this.height);
+        c.beginPath();
+        c.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
+        c.fill();
 
+        // red bar is behind green and shows up when health "decreases" by shortening green bar
+        // health bar
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y -15, this.width, 10)
+
+        // health bar
+        c.fillStyle = 'green'
+        if(this.health > 0){
+            this.width * this.health/100
+        } else{
+            this.health = 0
+        }
+        c.fillRect(this.position.x, this.position.y -15, this.health, 10)
     }
 
     update() {
@@ -77,14 +95,76 @@ class Enemy {
     }
 }
 
+class Projectile {
+    constructor({ x = 0, y = 0 }, enemy) {
+        this.position = { x, y };
+        this.velocity = {
+            x: 0,
+            y: 0
+        };
+        this.enemy = enemy;
+        this.radius = 10;
+        this.power = 2;
+
+    }
+    draw() {
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+        c.fillStyle = 'orange';
+        c.fill();
+    }
+
+    update() {
+        this.draw();
+        const angle = Math.atan2(
+            this.enemy.center.y - this.position.y,
+            this.enemy.center.x - this.position.x);
+
+        this.velocity.x = Math.cos(angle) * this.power;
+        this.velocity.y = Math.sin(angle) * this.power;
+
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    }
+}
+
 class Building {
     constructor({ x = 0, y = 0 }) {
         this.position = { x, y };
         this.width = 64 * 2;
+        this.height = 64;
+        this.center = {
+            x: this.position.x + this.width / 2,
+            y: this.position.y + this.height / 2
+        };
+        this.projectiles = [
 
+        ];
+        this.radius = 250; // radius used for distance that towers must be to start shooting at enemy
+        this.target;
+        this.frames = 0;
     }
     draw() {
         c.fillStyle = 'blue';
         c.fillRect(this.position.x, this.position.y, this.width, 64);
+
+        c.beginPath();
+        c.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
+        c.fillStyle = 'rgba(0,0,255,0.2)';
+        c.fill();
+    }
+    update() {
+        this.draw();
+        // add a projectile being fired every n=100 frames at an enemy that is in range
+        if (this.frames % 100 === 0 && this.target) {
+            this.projectiles.push(new Projectile(
+                {
+                    x: this.center.x,
+                    y: this.center.y
+                },
+                this.target
+            ));
+        }
+        this.frames++;
     }
 }
--- tower-defense/js/index.js
+++ tower-defense/js/index.js
@@ -23,9 +23,6 @@ for (let i = 0; i < placementTilesData.length; i += 20) {
     placementTilesData2d.push(placementTilesData.slice(i, i + 20));
 }
 
-
-
-
 placementTilesData2d.forEach((row, y) => {
     row.forEach((symbol, x) => {
         if (symbol === 14) {
@@ -40,9 +37,6 @@ placementTilesData2d.forEach((row, y) => {
 });
 
 
-
-
-
 const image = new Image();
 image.src = '../assets/game-map.png';
 
@@ -50,18 +44,25 @@ let x = 200;
 let y = 400;
 
 const enemies = [];
-for (let i = 0; i < 10; i++) {
-    const xOffset = i * 150;
-    // console.log(xOffset);
+let enemyCount = 3;
+spawnEnemies(enemyCount);
+function spawnEnemies(spawnCount) {
+    spawnCount = spawnCount + 1;
 
-    let xStart = waypoints[0].x - xOffset;
-    console.log(xStart);
+    for (let i = 1; i < spawnCount; i++) {
+        let xOffset = i * 150;
+        let xStart = waypoints[0].x - xOffset;
+        let yStart = waypoints[0].y;
+        let enemy = new Enemy({ x: xStart, y: yStart });
 
-    let enemy = new Enemy({ x: waypoints[0].x - xOffset, y: waypoints[0].y });
-    console.log(enemy.position.x);
-    enemies.push(enemy);
+        enemies.push(enemy);
+    }
 
 }
+
+
+
+
 const buildings = [];
 let activeTile = undefined;
 
@@ -70,15 +71,71 @@ function animate() {
     requestAnimationFrame(animate);
     c.drawImage(image, 0, 0);
 
-    enemies.forEach(enemy => {
+    for (let i = enemies.length - 1; i >= 0; i--) {
+        const enemy = enemies[i];
         enemy.update();
-    });
+    }
+
+
     placementTiles.forEach(tile => {
         tile.update(mouse);
     });
-    buildings.forEach(building => {
-        building.draw();
-    });
+
+    for (let i = buildings.length - 1; i >= 0; i--) {
+        const building = buildings[i];
+        building.update();
+        building.target = null;
+        const validEnemies = enemies.filter(enemy => {
+            const xDifference = enemy.center.x - building.position.x;
+            const yDifference = enemy.center.y - building.position.y;
+            const distance = Math.hypot(xDifference, yDifference);
+            // if the distance between an enemy and the building is inside the targeting area
+            // then the enemy can be fired at
+            return distance < enemy.radius + building.radius;
+
+        });
+        building.target = validEnemies[0];
+
+        for (let i = building.projectiles.length - 1; i >= 0; i--) {
+            const projectile = building.projectiles[i];
+
+            projectile.update();
+            const xDifference = projectile.enemy.center.x - projectile.position.x;
+            const yDifference = projectile.enemy.center.y - projectile.position.y;
+            const distance = Math.hypot(xDifference, yDifference);
+
+            // Projectile Hits enemy. Eliminate projectile and reduce enemy health
+            if (distance < projectile.enemy.radius + projectile.radius) {
+
+                // ENEMY HEALTH
+                projectile.enemy.health -= 20;
+                if (projectile.enemy.health <= 0) {
+                    // find enemy that has been killed
+                    const enemyIndex = enemies.findIndex((enemy) => {
+                        return projectile.enemy === enemy;
+                    });
+                    // cover case where enemy killed by another projectile while others are still on
+                    // their way, ie prevents removing additonal enemies from array
+                    // because the enemy was killed before all projectiles targeting it actually hit
+                    // REMOVE ENEMY
+                    if (enemyIndex > -1) {
+                        enemies.splice(enemyIndex, 1);
+                    }
+
+                    if (enemies.length === 0) {
+                        enemyCount += 2;
+                        spawnEnemies(enemyCount);
+                    }
+
+
+
+                }
+                // REMOVE PROJECTILE AFTER IT HITS ENEMY
+                building.projectiles.splice(i, 1);
+            }
+        }
+
+    }
 
 }
 
@@ -91,7 +148,7 @@ canvas.addEventListener('click', (event) => {
             y: activeTile.position.y
         }));
         activeTile.isOccupied = true;
-        console.log(buildings);
+
     }
 });
 
@@ -111,6 +168,6 @@ window.addEventListener('mousemove', (event) => {
             break;
         }
     }
-    // console.log(activeTile);
+
 
 });
